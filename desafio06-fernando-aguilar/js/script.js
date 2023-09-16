35a7a0a06773544c4f4f18945483303add14dacf
Fernando Aguilar <you@example.com>
2023-09-16 16:12:45 -0300

    Ejercicio 1 y 2 en proceso

--- desafio06-fernando-aguilar/js/script.js
+++ desafio06-fernando-aguilar/js/script.js
@@ -1,39 +1,45 @@
 console.log("Bienvenidos al Ejercicio 1 del Desafio 6");
-let catNum = prompt("Ingrese la cantidad de Gatos que desea ver: ") ;
+let catNum = prompt("Ingrese la cantidad de Gatos que desea ver: ",
+"Para finalizar ingrese 0 o un valor mayor a 10");
 console.log("El valor ingresado es:", catNum);
-for (let i = 1; i <= catNum; i++) {
-    catCounter = i;
-    console.log(catCounter);
-    switch (catCounter) {
-        case 1: console.log("Gato #1: 😺");
+
+
+while (catNum > 0 && catNum <= 10) {
+        switch (catNum) {
+                case "1": 
+                console.log("Gato #1: 😺");
                 break;
-        case 2: console.log("Gato #1: 😺");
+                case "2": 
+                console.log("Gato #1: 😺");
                 console.log("Gato #2: 😸");
                 break;
-                
-        case 3: console.log("Gato #1: 😺");
+                case "3": 
+                console.log("Gato #1: 😺");
                 console.log("Gato #2: 😸");
                 console.log("Gato #3: 😹");
                 break;
-        case 4: console.log("Gato #1: 😺");
+                case "4": 
+                console.log("Gato #1: 😺");
                 console.log("Gato #2: 😸");
                 console.log("Gato #3: 😹");
                 console.log("Gato #4: 😺");
                 break;
-        case 5: console.log("Gato #1: 😺");
+                case "5": console.log("Gato #1: 😺");
                 console.log("Gato #2: 😸");
                 console.log("Gato #3: 😹");
                 console.log("Gato #4: 😺");
                 console.log("Gato #5: 😸");
                 break;
-        case 6: console.log("Gato #1: 😺");
+                case "6": 
+                console.log("Gato #1: 😺");
                 console.log("Gato #2: 😸");
                 console.log("Gato #3: 😹");
                 console.log("Gato #4: 😺");
                 console.log("Gato #5: 😸");
                 console.log("Gato #6: 😹");
                 break;
-        case 7: console.log("Gato #1: 😺");
+                case "7": 
+                console.log("Gato #1: 😺");
                 console.log("Gato #2: 😸");
                 console.log("Gato #3: 😹");
                 console.log("Gato #4: 😺");
@@ -41,10 +47,48 @@ for (let i = 1; i <= catNum; i++) {
                 console.log("Gato #6: 😹");
                 console.log("Gato #7: 😺");
                 break;
-        
-        default:
-            break;
-    }
+                case "8": 
+                console.log("Gato #1: 😺");
+                console.log("Gato #2: 😸");
+                console.log("Gato #3: 😹");
+                console.log("Gato #4: 😺");
+                console.log("Gato #5: 😸");
+                console.log("Gato #6: 😹");
+                console.log("Gato #7: 😺");
+                console.log("Gato #8: 😸");
+                break;
+                case "9": 
+                console.log("Gato #1: 😺");
+                console.log("Gato #2: 😸");
+                console.log("Gato #3: 😹");
+                console.log("Gato #4: 😺");
+                console.log("Gato #5: 😸");
+                console.log("Gato #6: 😹");
+                console.log("Gato #7: 😺");
+                console.log("Gato #8: 😸");
+                console.log("Gato #9: 😹");
+                break;
+                case "10": 
+                console.log("Gato #1: 😺");
+                console.log("Gato #2: 😸");
+                console.log("Gato #3: 😹");
+                console.log("Gato #4: 😺");
+                console.log("Gato #5: 😸");
+                console.log("Gato #6: 😹");
+                console.log("Gato #7: 😺");
+                console.log("Gato #8: 😸");
+                console.log("Gato #9: 😹");
+                console.log("Gato #10: 😺");
+                break;
+                
+                default:
+                break;
+                }
+                catNum = prompt("Ingrese la cantidad de Gatos que desea ver: ", 
+                "Para finalizar ingrese 0 o un valor mayor a 10") ;
+                console.log("El valor ingresado es:", catNum);
+};
+
+if (catNum < 1 || catNum > 10){
+        alert("Finalizamos el juego!")
 }
-
-
---
+++ desafio06-fernando-aguilar/js/script2.js
@@ -0,0 +1,41 @@
+console.log("Bienvenidos al Ejercicio 2 del Desafio 6");
+let catNum = prompt("Ingrese la cantidad de Gatos que desea ver: ");
+console.log("El valor ingresado para la cantidad de gatos es:", catNum);
+
+let catSteps = prompt("Ingrese la cantidad de pasos que desea ver: ",
+"Para finalizar ingrese 0 o un valor mayor a 10");
+console.log("El valor ingresado para la cantidad de pasos es:", catSteps);
+
+let cats;
+let steps;
+
+switch(catNum) {
+    case "1":
+        cats = "🐈"
+        break;
+    case "2":
+        cats = "🐈🐈"
+        break;
+    case "3":
+        cats = "🐈🐈🐈"
+        break;
+    case "4":
+        cats = "🐈🐈🐈🐈"
+        break;
+    case "5":
+        cats = "🐈🐈🐈🐈🐈"
+        break;
+    case "":
+        break;
+    case "":
+        break;
+    case "":
+        break;
+    case "":
+        break;
+    case "":
+        break;
+
+}
+
+
