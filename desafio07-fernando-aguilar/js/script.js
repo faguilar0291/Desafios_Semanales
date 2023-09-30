5912d4d9d20149bd56c83933fe68662dda118561
Fernando Aguilar <you@example.com>
2023-09-30 15:17:49 -0300

    Ejercicio 7 en progreso, 2

--- desafio07-fernando-aguilar/js/script.js
+++ desafio07-fernando-aguilar/js/script.js
@@ -2,3 +2,5 @@
 
 let title = document.querySelector(".title_ejercicio") //Buscamos el texto dentro del Title
 console.log(title.innerHTML) // Mostramos por consola el texto dentro del Title
+
+let leerNombres = document.querySelector()
