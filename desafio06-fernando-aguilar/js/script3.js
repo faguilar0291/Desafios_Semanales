e82bfa498f90c0a6a82ea48e3a6435985241a562
Fernando Rodrigo Aguilar Dorado <altekfer@gmail.com>
2023-09-29 23:45:17 -0300

    Corrigiendo el Desafio 6, Ejercicio 3

--- desafio06-fernando-aguilar/js/script3.js
+++ desafio06-fernando-aguilar/js/script3.js
@@ -1,66 +1,34 @@
 console.log("Bienvenidos al Ejercicio 2 del Desafio 6");
-let catNum = prompt("Ingrese la cantidad de Gatos que desea ver: ");
+
+let catNum = prompt("Ingrese la cantidad de Gatos que desea ver: ",
+    "Para finalizar ingrese 0");
 console.log("El valor ingresado para la cantidad de gatos es:", catNum);
 
-let catSteps = prompt("Ingrese la cantidad de pasos que desea ver: ",
-    "Para finalizar ingrese 0 o un valor mayor a 10");
-console.log("El valor ingresado para la cantidad de pasos es:", catSteps);
+let catSteps;
 
-let cat_1 = "🐈";
-let cat_2 = "🐈‍⬛";
-let steps;
-
-while (catSteps > 0 && catSteps <= 10) {
-
-    switch (catSteps) {
-        case "1":
-            steps = "🐾"
-            break;
-        case "2":
-            steps = "🐾🐾"
-            break;
-        case "3":
-            steps = "🐾🐾🐾"
-            break;
-        case "4":
-            steps = "🐾🐾🐾🐾"
-            break;
-        case "5":
-            steps = "🐾🐾🐾🐾🐾"
-            break;
-        case "6":
-            steps = "🐾🐾🐾🐾🐾🐾"
-            break;
-        case "7":
-            steps = "🐾🐾🐾🐾🐾🐾🐾"
-            break;
-        case "8":
-            steps = "🐾🐾🐾🐾🐾🐾🐾🐾"
-            break;
-        case "9":
-            steps = "🐾🐾🐾🐾🐾🐾🐾🐾🐾"
-            break;
-        case "10":
-            steps = "🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾"
-            break;
-        default:
-            break;
-
-    }
-
-    for (i = 1; i <= catNum; i++) {
-        if ((i % 2) == 0){
-            console.log("Gato #", i, ":", cat_2, steps);
-        } else {
-            console.log("Gato #", i, ":", cat_1, steps);
-        }
-    }
-
-    alert("Excelente! Para volver a jugar recarga la página!")
-
-    break;
+if (catNum == 0) {
+    console.log("Ha ingresado 0, finalizamos el juego!");
+    document.write("<p> Ha ingresado 0, finalizamos el juego! </p>");
+} else {
+    catSteps = prompt("Ingrese la cantidad de pasos que desea ver: ");
+    console.log("El valor ingresado para la cantidad de pasos es:", catSteps);
 }
 
-if (catSteps < 1 || catSteps > 10) {
-    alert("Finalizamos el juego!")
+let cats = "";
+
+if (catNum > 0 && catSteps > 0) {
+
+    for (let i = 1; i <= catNum; i++) {
+        let steps = "";
+        if (i % 2 != 0) {
+            cats = "🐈‍";
+        } else {
+            cats = "🐈‍⬛";
+        }
+        for (let j = 0; j < catSteps; j++) {
+            steps += "🐾";
+        }
+        console.log("Gato #", i, ": ", cats, steps);
+        document.write("<p> Gato #", i, ": ", cats, steps, " </p>")
+    }
 }
