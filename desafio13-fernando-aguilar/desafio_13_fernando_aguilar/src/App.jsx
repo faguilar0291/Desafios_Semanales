56a4bcd0593ba408a09ed7a54934fcafb5254d8a
Fernando Rodrigo Aguilar Dorado <altekfer@gmail.com>
2024-02-05 23:57:09 -0300

    Avances 003 - Desafio 13

--- desafio13-fernando-aguilar/desafio_13_fernando_aguilar/src/App.jsx
+++ desafio13-fernando-aguilar/desafio_13_fernando_aguilar/src/App.jsx
@@ -1,7 +1,6 @@
 import "./app.scss";
-import { useState } from "react";
-import TasksForm from "./TaskForm";
-import TasksList from "./TaskList";
+import { useState} from "react";
+
 
 //clase 43 2:15
 function App() {
@@ -15,6 +14,51 @@ function App() {
             </>
         )
     }
+
+    const TasksForm = () => {
+
+        const inputTask = (task) => {
+            
+        }
+
+        const btnAddTask = (e) => {
+            e.preventDefault();
+            
+        }
+
+        return (
+        <>
+            <form className="form__taskForm">
+                <input className="input__taskForm"
+                type="text" placeholder="New Task"
+                onChange={(e) => inputTask(e.target.value)}/>
+                <button className="btn__taskForm" onClick={btnAddTask}>Add Task</button>
+            </form>
+        </>
+        );
+    };
+
+
+    const TasksList = () => {
+
+        const [taskFormList, setTaskFormList] = useState([ "Hacer ejercicio 12", "Hacer ejercicio 14"]);
+        console.log(taskFormList);
+
+        return (
+        <>
+            <section className="section__task-list">
+                <ul className="section__ul">
+                    {taskFormList.map( (task, index) => (
+                        <li key={index} className="ul__li">
+                            <p>{task}</p>
+                            <button onClick={ () => deleteTask(index)}>End Task</button>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </>
+        );
+    };
 
     const Footer = () => {
         return (
@@ -31,12 +75,14 @@ function App() {
         <>
             <Header />
             <main className="main">
-                    <TasksForm />
-                    <TasksList />
+                <h1>Tasks List:</h1>
+                <TasksForm />
+                <TasksList />
             </main>
             <Footer />
         </>
     );
+
 }
 
 export default App;
