7adff925a3f58c09f38306dd0333e2d08f977b4b
Fernando Rodrigo Aguilar Dorado <altekfer@gmail.com>
2024-04-02 15:16:07 -0300

    Desafio 15 - Avance 002

--- desafio15-fernando-aguilar/desafio15-fernando-aguilar/src/components/layout/Container.jsx
+++ desafio15-fernando-aguilar/desafio15-fernando-aguilar/src/components/layout/Container.jsx
@@ -11,6 +11,7 @@ export default function Container( ) {
 
     const [ cities, setCities] = useState([]);
     const [ weather, setWeather] = useState(null);
+    //const [ message, setMessage] = useState(null);
 
     useEffect( () => {
         (async () => {
@@ -18,22 +19,57 @@ export default function Container( ) {
         })();
     }, []);
 
+    const handleCities = async e => e.currentTarget.value && setWeather(await getCityWeather(e.currentTarget.value));
 
-    const handleCities = async e => e.currentTarget.value && setWeather(await getCityWeather(e.currentTarget.value))
+    //Intente usar el handleMessage para obtener el dato de la temperatura cuando este apareciera
+    // y así mostrar un mensaje según la temperatura pero no he logrado hacerlo funcionar
 
-    console.log(weather);
+    //prueba 1
+
+    // const handleMessage = ( (e) => {
+    //     if (e > 30) {
+    //         setMessage("Hace mucho calor! 🥵");
+    //     } else if (e < 10) {
+    //         setMessage("Hace mucho frio! 🥶");
+    //     } else {
+    //         setMessage(null);
+    //     }
+    //})
+
+    //prueba 2 no puedo obtener el valor cuando se renderiza la temperatura
+
+    //const handleMessage = e => e.currentTarget.value && console.log(e.currentTarget.value);
 
     return (
         <Box className="container">
             <h2 className="container__title">Clima actual por ciudad</h2>
             <ClimaCiudad>
-                <label>Elige una ciudad para saber el clima</label>
                 <select name="city-select" onChange={handleCities}>
                     <option value="">Elige una ciudad</option>
                     {cities.map( city => <option key={city.id} value={city.name}>{city.name}</option>)}
                 </select>
                 <Box>
-                    <h2>Temperatura actual: {weather.main.temp}</h2>
+                    
+                    {/* opción de renderizado 1
+                    {weather && (
+                        <h2>Temperatura actual: <span onChange={handleMessage} value={weather.main.temp}>{weather.main.temp + '°C'}</span></h2>
+                    )} */}
+                    {weather && (
+                        <h2>Temperatura actual: {weather.main.temp + '°C'}</h2>
+                    )}
+                    
+                    {/* opción de renderizado 1
+                    {message && (
+                        <h2>{message}</h2>
+                    )} */}
+                    
+                    {/* Esta opción de renderizado si me funciona pero no hago uso de una variable de estado */}
+                    {weather && weather.main.temp > 30 && (
+                        <h2>Hace mucho calor! 🥵</h2>
+                    )}
+                    {weather && weather.main.temp < 10 && (
+                        <h2>Hace mucho frio! 🥶</h2>
+                    )}
                 </Box>
             </ClimaCiudad>
         </Box>
