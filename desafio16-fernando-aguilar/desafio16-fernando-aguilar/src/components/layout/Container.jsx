0e3d07f558c31c3405cb643b81dd75a8181c1083
Fernando Rodrigo Aguilar Dorado <altekfer@gmail.com>
2024-05-05 22:54:53 -0300

    Desafio 16 - Avance 004

--- desafio16-fernando-aguilar/desafio16-fernando-aguilar/src/components/layout/Container.jsx
+++ desafio16-fernando-aguilar/desafio16-fernando-aguilar/src/components/layout/Container.jsx
@@ -4,41 +4,84 @@ import { products } from "../../data/products";
 import useLocalStorage from "../hooks/useLocalStorage";
 import ProductList from "./ProductList"
 import ProductForm from "./ProductForm";
-import { jsx } from "@emotion/react";
+import { useState } from "react";
 
 //51:34
 
 const Container = () => {
 
-    const localStorage = useLocalStorage({ count: 0, products});
+    const localStorage = useLocalStorage({products});
 
     const handleRemoveItem = (key) => {
 
-        console.log(key);
-        
         console.log(localStorage.value.products);
 
         const productos = localStorage.value.products;
 
-        console.log(productos[0]);
+        console.log(productos[0]); // verifico que cargó la info de localstorage
 
         const filteredProducts = productos.filter(product => product.name !== key);
 
-        console.log(filteredProducts);
+        localStorage.setItemValue('products', filteredProducts);
+    };
 
-        localStorage.setItemValue('products', JSON.stringify(filteredProducts));
+    // const [inputProductName , setInputProductName] = useState("");
+    // const [inputDescription , setInputDescription] = useState("");
+    // const [InputPrice , setInputPrice] = useState("");
 
-        // console.log(localStorage);
-    }
+    // const generateId = () => {
+    //      let maxId = 0;
+    //      const productos = localStorage.value.products;
+    //      productos.forEach( (product) => {
+    //      if (product.id > maxId) {
+    //          maxId = product.id;
+    //          }
+    //      });
+    //      return maxId + 1;
+    // };
+
+    // const handleProductName = (e) => {
+    //     setInputProductName(e.target.value);
+    // };
+
+    // const handleProductDescription = (e) => {
+    //     setInputDescription(e.target.value);
+    // };
+
+    // const handleProductPrice = (e) => {
+    //     setInputPrice(e.target.value);
+    // };
+
+    //  const addTaskBtn = () => {
+    //      if (inputProductName.trim().length > 0) {
+    //      // Crea una nueva tarea con un id nuevo (único)
+    //          const newProduct = {
+    //              id: generateId(),
+    //              title: inputProductName,
+    //              description: inputDescription,
+    //              price: InputPrice,
+    //         };
+
+    //      // Actualiza la lista de tareas
+    //      localStorage.setItemValue('products', newProduct);
+    //      setTaskArray([...taskArray, newTask]);
+
+    //         // Resetear el input de "Nueva tarea"
+    //         setInputProductName("");
+    //         setInputDescription("");
+    //         setInputPrice("");
+    //     }
+
+    // };
+
 
 
     return (
         <Box className="container">
-                {/* CONTADOR - Versión custom hook useLocalStorage */}
+                {/* custom hook useLocalStorage */}
                 <Box>
                     <Box>
                         <button onClick={() => localStorage.clearItems()}>Clear</button>
-                        <button onClick={() => localStorage.removeItem("count")}>Remove</button>
                     </Box>
                 </Box>
             <h2 className="container__title">Lista de productos</h2>
@@ -54,9 +97,24 @@ const Container = () => {
             </ProductList>
             <ProductForm>
                 <h2>Creación de productos</h2>
+                {/* <input
+                    type="text"
+                    id='newName'
+                    placeholder='Nombre'
+                    value={inputProductName}
+                    onChange={(e) => handleProductName(e)}
+                    className='form-input'/>
                     <input type="text"
-                        onChange={ (e) => createName(e)}
-                    />
+                    placeholder='Descripción'
+                    value={inputDescription}
+                    onChange={(e) => handleProductDescription(e)}
+                    className='form-input'/>
+                    <input type="number"
+                    placeholder='Precio'
+                    value={inputPrice}
+                    onChange={(e) => handleProductPrice(e)}
+                    className='form-input'/>
+                <button onClick={} className='form-btn material-symbols-outlined'>Agregar Producto</button> */}
             </ProductForm>
         </Box>
     );
--- desafio16-fernando-aguilar/desafio16-fernando-aguilar/src/data/products.js
+++ desafio16-fernando-aguilar/desafio16-fernando-aguilar/src/data/products.js
@@ -1,13 +1,27 @@
 export const products = [
     {
+        id: 1,
         name: "Play Station 5",
         description: "Consola de video juegos",
         price: 1300,
 
     },
     {
+        id: 2,
         name: "Play Station 6",
         description: "Consola de video juegos",
         price: 1700,
+    },
+    {
+        id: 3,
+        name: "Nintendo 64",
+        description: "Consola de video juegos",
+        price: 1200,
+    },
+    {
+        id: 4,
+        name: "Nintendo 64 - 2",
+        description: "Consola de video juegos",
+        price: 1600,
     },
 ]
